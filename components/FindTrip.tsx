58752406a67655ff13477493d29488982013fc64
tornikeokroadze <tornikeoqroadze@gmail.com>
2025-02-28 14:23:00 +0400

    Corrected filter for mobile design

--- components/FindTrip.tsx
+++ components/FindTrip.tsx
@@ -28,14 +28,14 @@ export default function FindTrip() {
   };
 
   return (
-    <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 lg:w-auto w-full h-96 lg:h-auto px-4 lg:px-0">
+    <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 lg:w-auto w-full h-96 lg:h-auto px-2 lg:px-0">
       <h1 className="text-white text-3xl lg:text-7xl font-semibold text-center mb-6">
         Find Your Trip
       </h1>
 
       <form
         onSubmit={handleSearch}
-        className="flex flex-col lg:flex-row p-4 lg:p-0"
+        className="flex flex-col lg:flex-row p-2 lg:p-0"
       >
         <div className="grid grid-cols-2 lg:flex shadow-lg rounded-t-lg lg:rounded-l-lg lg:rounded-tr-none overflow-hidden bg-white">
           {/* Direction Selection */}
@@ -73,7 +73,7 @@ export default function FindTrip() {
           </div>
 
           {/* Start Date */}
-          <div className="flex flex-col w-full lg:w-48 h-24 border-r border-gray-300 px-6 justify-center">
+          <div className="flex flex-col w-full lg:w-48 h-20 lg:h-24 border-r border-gray-300 px-6 justify-center">
             <span className="text-sm text-gray-500 mb-2">Start Date</span>
             <input
               type="date"
@@ -84,7 +84,7 @@ export default function FindTrip() {
           </div>
 
           {/* End Date */}
-          <div className="flex flex-col w-full lg:w-48 h-24 border-gray-300 px-6 justify-center">
+          <div className="flex flex-col w-full lg:w-48 h-20 lg:h-24 border-gray-300 px-6 justify-center">
             <span className="text-sm text-gray-500 mb-2">End Date</span>
             <input
               type="date"
